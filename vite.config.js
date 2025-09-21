23132e02a9182692a27d7fccbbc969ac486d4518
Marat <maratmirzabalaev@gmail.com>
2025-09-21 03:55:45 +0300

    e

--- vite.config.js
+++ vite.config.js
@@ -2,24 +2,13 @@ import { defineConfig } from 'vite'
 import path from 'path'
 
 export default defineConfig({
-    root: path.resolve(__dirname, 'src'), // Корень - папка src
+    root: path.resolve(__dirname, 'src'),
     build: {
-        outDir: '../dist', // Выходная директория на уровень выше
+        outDir: '../dist',
         emptyOutDir: true,
         rollupOptions: {
-            input: {
-                main: path.resolve(__dirname, 'src/index.html'), // Полный путь
-                str3: path.resolve(__dirname, 'src/index3.html') // Полный путь
-            },
             output: {
-                manualChunks: undefined,
-                // Опционально: переименовать выходные файлы
-                assetFileNames: (assetInfo) => {
-                    if (assetInfo.name === 'index3.html') {
-                        return 'str3.html'; // Переименовываем index2.html в str3.html
-                    }
-                    return 'assets/[name]-[hash][extname]';
-                }
+                manualChunks: undefined
             }
         },
         target: 'es2015'
