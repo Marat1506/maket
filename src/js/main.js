11ebb32ef5b85657fa92dc7e16d6a5585b0143d9
Marat <maratmirzabalaev@gmail.com>
2025-09-21 21:46:35 +0300

    dsf

--- src/js/main.js
+++ src/js/main.js
@@ -5,54 +5,54 @@ import '../scss/styles.scss'
 import * as bootstrap from 'bootstrap'
 
 // Простая логика маршрутизации
-function checkRoute() {
-    const currentPath = window.location.pathname;
+// function checkRoute() {
+//     const currentPath = window.location.pathname;
     
-    if (currentPath === '/str') {
-        // Загружаем содержимое index2.html и вставляем в body
-        fetch('./index2.html')
-            .then(response => response.text())
-            .then(html => {
-                // Извлекаем содержимое body из index2.html
-                const parser = new DOMParser();
-                const doc = parser.parseFromString(html, 'text/html');
-                const newBodyContent = doc.body.innerHTML;
+//     if (currentPath === '/str') {
+//         // Загружаем содержимое index2.html и вставляем в body
+//         fetch('./index2.html')
+//             .then(response => response.text())
+//             .then(html => {
+//                 // Извлекаем содержимое body из index2.html
+//                 const parser = new DOMParser();
+//                 const doc = parser.parseFromString(html, 'text/html');
+//                 const newBodyContent = doc.body.innerHTML;
                 
-                // Заменяем содержимое текущего body
-                document.body.innerHTML = newBodyContent;
+//                 // Заменяем содержимое текущего body
+//                 document.body.innerHTML = newBodyContent;
                 
-                // Обновляем заголовок страницы
-                document.title = doc.title;
-            })
-            .catch(error => {
-                console.error('Ошибка загрузки index2.html:', error);
-            });
-    }
+//                 // Обновляем заголовок страницы
+//                 document.title = doc.title;
+//             })
+//             .catch(error => {
+//                 console.error('Ошибка загрузки index2.html:', error);
+//             });
+//     }
 
-    if (currentPath === '/str3') {
-        // Загружаем содержимое index2.html и вставляем в body
-        fetch('./index3.html')
-            .then(response => response.text())
-            .then(html => {
-                // Извлекаем содержимое body из index2.html
-                const parser = new DOMParser();
-                const doc = parser.parseFromString(html, 'text/html');
-                const newBodyContent = doc.body.innerHTML;
+//     if (currentPath === '/str3') {
+//         // Загружаем содержимое index2.html и вставляем в body
+//         fetch('./index3.html')
+//             .then(response => response.text())
+//             .then(html => {
+//                 // Извлекаем содержимое body из index2.html
+//                 const parser = new DOMParser();
+//                 const doc = parser.parseFromString(html, 'text/html');
+//                 const newBodyContent = doc.body.innerHTML;
 
-                // Заменяем содержимое текущего body
-                document.body.innerHTML = newBodyContent;
+//                 // Заменяем содержимое текущего body
+//                 document.body.innerHTML = newBodyContent;
 
-                // Обновляем заголовок страницы
-                document.title = doc.title;
-            })
-            .catch(error => {
-                console.error('Ошибка загрузки index2.html:', error);
-            });
-    }
-}
+//                 // Обновляем заголовок страницы
+//                 document.title = doc.title;
+//             })
+//             .catch(error => {
+//                 console.error('Ошибка загрузки index2.html:', error);
+//             });
+//     }
+// }
 
 // Проверяем маршрут при загрузке страницы
-checkRoute();
+// checkRoute();
 
 // Динамическая подгрузка header.html
 // fetch('./components/header.html')
